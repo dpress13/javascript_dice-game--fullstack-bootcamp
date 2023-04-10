d6edb82267631d6ae00d99b17f5343edcccbdf2f
dan-o <66097571+dpress13@users.noreply.github.com>
2023-04-10 11:12:05 -0400

    clean up each file

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+// Create two random numbers <= 6
+const randomNum1 = Math.floor(Math.random() * 6) + 1;
+const randomNum2 = Math.floor(Math.random() * 6) + 1;
 
-function allFunctions() {
-    rollDice(randomNumber1, randomNumber2);
-    winner();
+// Replace respective players' dice with the image that matches its random number
+const rollDice = (randomNum1, randomNum2) => {
+    document.getElementById("player1").setAttribute("src", "images/dice" + randomNum1 + ".png");
+    document.getElementById("player2").setAttribute("src", "images/dice" + randomNum2 + ".png");
 }
 
-function rollDice(randomNumber1, randomNumber2) {
-    document.querySelector("img.img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
-    document.querySelector("img.img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
-}
-
-function winner() {
-    if (randomNumber1 > randomNumber2) {
-        document.querySelector("h1").innerText = "Player 1 Wins! 🚩";
-    } else if (randomNumber1 < randomNumber2) {
+// Print which player is the winner
+const winner = () => {
+    if (randomNum1 > randomNum2) {
+        document.querySelector("h1").innerText = "🚩Player 1 Wins!";
+    } else if (randomNum1 < randomNum2) {
         document. querySelector("h1").innerText = "🚩Player 2 Wins!";
     } else {
         document.querySelector("h1").innerText = "🚩It's A Draw!";
     }
 }
+
+// Roll dice and print which player is the winner
+const allFunctions = () => {
+    rollDice(randomNum1, randomNum2);
+    winner();
+}
+
 allFunctions();
